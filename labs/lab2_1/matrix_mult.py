a7b9a16b1e7392acee2daaf4a7f005e1b6704028
Egor Loshmanov <egorloshmanov06@gmail.com>
2025-09-16 17:56:07 +0700

    Fix: в лабе 2_1 исправлена основная логика перемножения матриц и было вынесено в отдельную функцию

--- labs/lab2_1/matrix_mult.py
+++ labs/lab2_1/matrix_mult.py
@@ -1,27 +1,50 @@
-matrix1, matrix2 = [], []
+def prod_matrix(matrix1: list[list[int]], matrix2: list[list[int]]) -> list[list[int]]:
+    """
+    Возвращает произведение двух матриц
 
-f = open("matrix.txt")
-for line in f:
-    if line[0] != '\n':
-     # Разделяем строку на элементы и преобразуем в числа
-        row = [int(x) for x in line.strip().split()]
-        matrix1.append(row)
-    else:
-        break
-
-for line in f:
-    row = [int(x) for x in line.strip().split()]
-    matrix2.append(row)
-
-m, k1, k2 = len(matrix1), len(matrix2[0]), len(matrix1[0])
-matrix3 = [[0 for _ in range(k1)] for _ in range(m)]
-
-for i in range(m):
-    for j in range(k1):
-        for z in range(k2):
-            matrix3[i][j] = matrix1[i][z] * matrix2[z][j]
-print(matrix3)
+    Parameters
+    ----------
+    matrix1 : list[list[int]]
+        Первая матрица размера m x n
+    matrix2 : list[list[int]]
+        Вторая матрица размера n x k
 
 
+    Returns
+    -------
+    result : list[list[int]]
+        Результат перемножения matrix1 и matrix2 - matrix3 размера m x k
+    """
+    matrix3 = [[0 for _ in range(len(matrix2[0]))] for _ in range(len(matrix1))]
+    for i in range(len(matrix1)):
+        for j in range(len(matrix2[0])):
+            for k in range(len(matrix2)):
+                matrix3[i][j] += matrix1[i][k] * matrix2[k][j]
+    return matrix3
 
+def main():
+    with open("input.txt") as f:
+        matrix1, matrix2 = [], []
+        for line in f:
+            if line[0] != '\n':
+                row = [int(x) for x in line.split()]
+                matrix1.append(row)
+            else:
+                break
 
+        for line in f:
+            row = [int(x) for x in line.split()]
+            matrix2.append(row)
+
+        if len(matrix1[0]) == len(matrix2):
+            matrix3 = prod_matrix(matrix1, matrix2)
+
+            with open("output.txt", "w") as f:
+                for i in matrix3:
+                    f.write(" ".join(map(str, i)))
+                    f.write('\n')
+        else:
+            print("Количество столбцов первой матрицы должно совпадать с количеством строк второй матрицы!")
+
+if __name__ == "__main__":
+    main()
